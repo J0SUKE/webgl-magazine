ca12e4faed0a48815fe074b4be81dfd6f91c35d9
Jean <chakib3500@gmail.com>
2025-09-14 21:13:32 +0200

    page rotation logic

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import Canvas from "./canvas"
 import Scroll from "./scroll"
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { $, $$ } from "./utils/dom"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -13,6 +14,19 @@ class App {
   constructor() {
     this.scroll = new Scroll()
     this.canvas = new Canvas({ scroll: this.scroll })
+
+    const pages = $$("[data-page]")
+    let currentPageIndex = 0
+    const container = $("[data-pages-container]")
+
+    document.addEventListener("click", (e) => {
+      pages[currentPageIndex].style.setProperty("--turn-page", "1")
+      currentPageIndex++
+      container.style.setProperty(
+        "--data-current-page-index",
+        currentPageIndex.toString()
+      )
+    })
 
     this.render()
   }
